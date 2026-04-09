c71be1f52e51d1ddaea439d29d44ccc8c2a05ec5
mohithgowda187 <mohithgowdahk06@gmail.com>
2026-04-09 07:09:22 +0530

    Update 16server.java

--- 16server.java
+++ 16server.java
@@ -12,3 +12,22 @@ class MyHandler(SimpleHTTPRequestHandler):
 server = HTTPServer(("0.0.0.0", PORT), MyHandler)
 print(f"Server running on port {PORT}...")
 server.serve_forever()
+
+
+#java
+import com.sun.net.httpserver.*;
+import java.net.InetSocketAddress;
+
+public class SimpleJavaServer {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(5020), 0);
+        server.createContext("/", exchange -> {
+            String response = "<h1>Hello from Docker Java Container</h1>";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+        server.start();
+        System.out.println("Server running on port 5020...");
+    }
+}
